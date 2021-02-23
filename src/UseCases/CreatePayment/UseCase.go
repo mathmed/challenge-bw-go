436115c4fef92ec052b62456b82a0ec335ec7524
Mateus Medeiros <mateusmedeiros252525@hotmail.com>
2021-02-23 19:06:02 -0300

    feat: save payment data on DB

--- src/UseCases/CreatePayment/UseCase.go
+++ src/UseCases/CreatePayment/UseCase.go
@@ -1,16 +1,27 @@
 package createpayment
 
 import (
-	"fmt"
+	"errors"
 
+	repositories "github.com/mathmed/challenge-bw-go/src/Infra/Database/Repositories"
 	pagseguro "github.com/mathmed/challenge-bw-go/src/Infra/PagSeguro"
+	custom_errors "github.com/mathmed/challenge-bw-go/src/Infra/PagSeguro/Errors"
 	dtos "github.com/mathmed/challenge-bw-go/src/UseCases/CreatePayment/Dtos"
 )
 
 // UseCase .
-func UseCase(payment dtos.CreatePayment)  {
+func UseCase(payment dtos.CreatePayment) error {
 
-	hash := pagseguro.CreatePaymentPagSeguro(payment)
-	fmt.Printf(hash)
-
+	var paymentStatus uint
+	hash, err := pagseguro.CreatePaymentPagSeguro(payment)
+	
+	if err != nil {
+		if (!errors.Is(err, &custom_errors.PaymentDeclinedError{})) {
+			return err
+		}
+		paymentStatus = 2
+	}
+	paymentStatus = 1
+	repositories.SavePayment(*payment.Card, hash, paymentStatus)
+	return nil
 }
