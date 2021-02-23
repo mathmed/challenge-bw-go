86f52de305c4fe6304a8201022287bc936a4a752
Mateus Medeiros <mateusmedeiros252525@hotmail.com>
2021-02-22 22:06:36 -0300

    refactor: use GIN

--- src/main.go
+++ src/main.go
@@ -3,17 +3,25 @@ package main
 import (
 	"log"
 	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"github.com/mathmed/challenge-bw-go/src/Routes"
 )
 
-
 func main() {
-	
+
+	godotenv.Load()
 	port := os.Getenv("PORT")
-	
+
 	if port == "" {
 		port = "8080"
 	}
 
+	r := gin.Default()
+	Routes.Setup(r)
+	r.Run(":" + port)
 	log.Printf("Listening on port %s", port)
 
 }
+
