b6e1302f497face58ebbf64221a24fc54e5f1e39
Mateus Medeiros <mateusmedeiros252525@hotmail.com>
2021-02-23 19:04:41 -0300

    feat: implement config database

---
+++ src/Infra/Database/Config.go
@@ -0,0 +1,31 @@
+package database
+
+import (
+	"os"
+
+	models "github.com/mathmed/challenge-bw-go/src/Infra/Database/Models"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+// Instance .
+var Instance *gorm.DB
+
+// Config .
+func Config() {
+
+	dsn := 
+		os.Getenv("DB_USERNAME") + ":" + os.Getenv("DB_PASSWORD") + 
+		"@tcp(" +os.Getenv("DB_HOST") + ":" + os.Getenv("DB_PORT")+ ")/" +
+		os.Getenv("DB_DATABASE")
+
+	var err error
+	Instance, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	Instance.AutoMigrate(models.PaymentStatus{}, models.Payment{})
+
+}
