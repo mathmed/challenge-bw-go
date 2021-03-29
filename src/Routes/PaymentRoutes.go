39951f2660920beb8e94fdc3efbc7fc6fe6424c6
Mateus Medeiros <mateusmedeiros252525@hotmail.com>
2021-03-29 11:25:25 -0300

    feat: add get payment details route

--- src/Routes/PaymentRoutes.go
+++ src/Routes/PaymentRoutes.go
@@ -2,11 +2,14 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
-	http "github.com/mathmed/challenge-bw-go/src/UseCases/CreatePayment/Http"
+	http_create_payment "github.com/mathmed/challenge-bw-go/src/UseCases/CreatePayment/Http"
+	http_payment_details "github.com/mathmed/challenge-bw-go/src/UseCases/PaymentDetails/Http"
 )
 
 // PaymentRoutes .
 func PaymentRoutes(route *gin.RouterGroup) {
 	group := route.Group("/payment")
-	group.POST("/", http.CreatePaymentController)
+
+	group.POST("/", http_create_payment.CreatePaymentController)
+	group.GET("/:id", http_payment_details.PaymentDetailsController)
 }
